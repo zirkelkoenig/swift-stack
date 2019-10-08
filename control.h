749aead2dca3630e232954a565117ec507da0f3e
Marvin Kipping <zirkelkoenig@users.noreply.github.com>
2019-10-08 20:36:44 +0200

    License clarification

--- control.h
+++ control.h
@@ -2,7 +2,7 @@
 Copyright (C) 2017 Marvin Kipping
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
-License version 2 as published by the Free Software Foundation.
+License as published by the Free Software Foundation; version 2 only.
 
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
